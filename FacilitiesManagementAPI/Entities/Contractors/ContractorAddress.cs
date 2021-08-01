ba9b06476807c5b9667f026134f10532cec745bd
Chris <alder83@hotmail.co.uk>
2021-08-01 18:39:48 +0100

    added ssl

---
+++ FacilitiesManagementAPI/Entities/Contractors/ContractorAddress.cs
@@ -0,0 +1,14 @@
+namespace FacilitiesManagementAPI.Entities.Contractors
+{
+    public class ContractorAddress
+    {
+        public int Id { get; set; }
+        public string BusinessName { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string County { get; set; }
+        public string PostCode { get; set; }
+        public string  PhoneNumber1 { get; set; }
+        public string PhoneNumber2 { get; set; }     
+    }
+}
---
+++ FacilitiesManagementAPI/Entities/Premises/Premises.cs
@@ -0,0 +1,9 @@
+namespace FacilitiesManagementAPI.Entities
+{
+    public class Premises
+    {
+        public int Id { get; set; }
+        public string PremiseName { get; set; }
+    
+    }
+}
